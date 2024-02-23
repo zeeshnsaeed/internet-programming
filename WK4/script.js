9c5f2cef7fe957709ddb0a892c89f8f30f9c5c0e
Zeeshan Saeed <zsaeed830@gmail.com>
2024-02-23 17:08:15 +0000

    WK4 exercise 3 completed

--- WK4/script.js
+++ WK4/script.js
@@ -14,7 +14,9 @@
 
 const myLoadFunction = () => {
     var el = document.getElementById('name')
+    var el2 = document.getElementById('quote')
     el.firstChild.nodeValue = 'Zeeshan Saeed'
+    el2.firstChild.nodeValue = 'The quick brown fox runs over the lazy dog'
 }
 
 document.addEventListener('DOMContentLoaded', myLoadFunction)
